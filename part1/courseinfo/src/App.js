4e2e23f4c248a987563ba51a036c8db49da8ac70
André <andre.katsuragawa@gmail.com>
2021-05-18 13:18:46 -0300

    Exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,54 +2,47 @@ const Header = (props) => <h1>{props.course}</h1>;
 
 const Part = (props) => (
   <p>
-    {props.part} {props.excercise}
+    {props.part.name} {props.part.excercises}
   </p>
 );
 
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} excercise={props.exercises1} />
-      <Part part={props.part2} excercise={props.exercises2} />
-      <Part part={props.part3} excercise={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </>
   );
 };
 
 const Total = (props) => (
   <p>
-    Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    Number of exercises{" "}
+    {props.part1.exercises + props.part2.exercises + props.part3.exercises}
   </p>
 );
 
 function App() {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div className="App">
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
-      <Total
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </div>
   );
 }
